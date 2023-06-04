1cbc317194e5747a57bbb77a283efc734d0f0d08
Roshan Karanth <roshankaranth@gmail.com>
2023-06-04 14:37:55 +0530

    npm modules

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const getNotes = require('./notes.js')
-console.log(getNotes())
+const validator = require('validator')
+
+// const getNotes = require('./notes.js')
+// console.log(getNotes())
+
+console.log(validator.isEmail('roshan@email.com'))
+console.log(validator.isURL('Google.com'))
