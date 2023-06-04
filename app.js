7486442ee282c1f0b8f9683bf796cfe8dd72c1e4
Roshan Karanth <roshankaranth@gmail.com>
2023-06-04 13:02:31 +0530

    Third Lecture 2-3

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-const fs = require('fs') // require function to Load in modules
-fs.writeFileSync('notes.txt', 'My name is Roshan.')
-
-//naming convention in the documentation (best practice)!!
-
-fs.appendFileSync('notes.txt', "I am 21 years old")
+const getNotes = require('./notes.js')
+console.log(getNotes())
