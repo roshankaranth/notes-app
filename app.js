6139e09913d883dd6b7c7f92649da798c4603c17
Roshan Karanth <roshankaranth@gmail.com>
2023-06-07 16:00:04 +0530

    Added properties title and body to add command

--- app.js
+++ app.js
@@ -7,15 +7,59 @@ yargs.version('1.1.0')
 
 
 // Create add command
-yargs.command({ command: 'add', describe: 'Add a new note', handler: function () { console.log("Adding a new note.") } })
+yargs.command(
+    {
+        command: 'add',
+        describe: 'Add a new note',
+        builder: {
+            title:
+            {
+                describe: 'Note title',
+                type: 'string',
+                demandOption: true
+
+            },
+
+            body: {
+                describe: 'Note body',
+                type: 'string',
+                demandOption: true
+            }
+        },
+        handler: function (argv) {
+            console.log(`Title : ${argv.title}\nBody: ${argv.body}`)
+        }
+    })
 
 //Create remove node
-yargs.command({ command: 'remove', describe: 'remove a note.', handler: function () { console.log('remove a note.') } })
+yargs.command(
+    {
+        command: 'remove',
+        describe: 'remove a note.',
+        handler: function () {
+            console.log('remove a note.')
+        }
+    })
 
 // Create a read command
-yargs.command({ command: 'read', describe: 'reading a note', handler: function () { console.log('reading a note') } })
+yargs.command(
+    {
+        command: 'read',
+        describe: 'reading a note',
+        handler: function () {
+            console.log('reading a note')
+        }
+    })
 
 // Create a list command
-yargs.command({ command: 'list', describe: 'listing all notes', handler: function () { console.log('Listing all notes.') } })
+yargs.command(
+    {
+        command: 'list',
+        describe: 'listing all notes',
+        handler: function () {
+            console.log('Listing all notes.')
+        }
+    })
 
 
+yargs.parse()
